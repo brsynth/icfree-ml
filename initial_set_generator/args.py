3d513002fa75f991f33c2f48f74181aff9186469
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2022-01-27 00:52:20 +0100

    Create args.py

---
+++ initial_set_generator/args.py
@@ -0,0 +1,34 @@
+from argparse import (
+    ArgumentParser
+    )
+
+
+def build_args_parser(
+        program,
+        description):
+
+    parser = ArgumentParser(
+        program,
+        description,
+    )
+
+    parser = add_arguments(parser)
+
+    return parser
+
+
+def add_arguments(parser):
+
+    parser.add_argument(
+        'input',
+        type=str,
+        help='Path to a .csv file containing CFPS parameters and features',
+    )
+
+    # parser.add_argument(
+    #     'output',
+    #     type=str,
+    #     help='Path to the csv output',
+    # )
+
+    return parser
