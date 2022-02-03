167c051fb5c6acfca209328e4acf75a2f723f75b
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2022-02-03 01:37:14 +0100

    Update module
    initial_set_generator now generates three TSV files:
    - "intial_set.tsv"
    - "normalizer.tsv"
    - "autofluorescence.tsv"

--- process_initialization/initial_set_generator/__main__.py
+++ process_initialization/initial_set_generator/__main__.py
@@ -5,11 +5,8 @@ from .initial_set_generator import (
     levels_array_generator,
     variable_concentrations_array_generator,
     fixed_concentrations_array_generator,
-    maximum_concentrations_sample_generator,
-    autofluorescence_sample_generator,
-    control_concentrations_array_generator,
-    initial_training_set_generator,
-    save_intial_training_set,
+    initial_plates_generator,
+    save_intial_plates,
 )
 
 from .args import build_args_parser
@@ -40,35 +37,44 @@ def main():
                                 variable_concentrations_array,
                                 maximum_fixed_concentrations)
 
-    maximum_concentrations_sample = maximum_concentrations_sample_generator(
-                                input_df)[1]
+    # maximum_concentrations_sample = maximum_concentrations_sample_generator(
+    #                             input_df)[1]
 
-    maximum_concentrations_dict = maximum_concentrations_sample_generator(
-                                input_df)[0]
+    # maximum_concentrations_dict = maximum_concentrations_sample_generator(
+    #                             input_df)[0]
 
-    autofluorescence_sample = autofluorescence_sample_generator(
-                            maximum_concentrations_dict)
+    # autofluorescence_sample = autofluorescence_sample_generator(
+    #                         maximum_concentrations_dict)
 
-    control_concentrations_array = control_concentrations_array_generator(
-                                maximum_concentrations_sample,
-                                autofluorescence_sample)
+    # control_concentrations_array = control_concentrations_array_generator(
+    #                             maximum_concentrations_sample,
+    #                             autofluorescence_sample)
 
-    initial_set = initial_training_set_generator(
+    initial_set = initial_plates_generator(
         variable_concentrations_array,
         fixed_concentrations_array,
-        control_concentrations_array,
         input_df)[0]
 
-    initial_set_without_goi = initial_training_set_generator(
+    normalizer_set = initial_plates_generator(
         variable_concentrations_array,
         fixed_concentrations_array,
-        control_concentrations_array,
         input_df)[1]
 
-    save_intial_training_set(
-        input_df,
+    autolfuorescence_set = initial_plates_generator(
+        variable_concentrations_array,
+        fixed_concentrations_array,
+        input_df)[2]
+
+    all_parameters = initial_plates_generator(
+        variable_concentrations_array,
+        fixed_concentrations_array,
+        input_df)[3]
+
+    save_intial_plates(
         initial_set,
-        initial_set_without_goi)
+        normalizer_set,
+        autolfuorescence_set,
+        all_parameters)
 
 
 if __name__ == "__main__":
