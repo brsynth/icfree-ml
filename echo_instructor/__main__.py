8c571fad168b8826eb196d36a4e0049a66d9cce8
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2022-02-22 03:23:02 +0100

    Update __main__.py
    Change parser description and functions names

--- echo_instructor/__main__.py
+++ echo_instructor/__main__.py
@@ -4,77 +4,90 @@ from .echo_instructor import (
     input_importer,
     volumes_array_generator,
     save_volumes_array,
-    volumes_dispatcher,
-    source_to_destination,
+    destination_plate_generator,
+    echo_instructions_generator,
     save_echo_instructions
 )
+
 
 from .args import build_args_parser
 
 
 def main():
     parser = build_args_parser(
-        program='robots_handler',
-        description='Convert concetrations tsv files into volumes tsv files')
+        program='echo_instructor',
+        description='Generates instructions for the Echo robot')
 
     args = parser.parse_args()
-    input_file = args.input
-    input_file_1 = args.input1
-    input_file_2 = args.input2
-    input_file_3 = args.input3
+    cfps_parameters = args.input
+    initial_set_concentrations = args.input1
+    normalizer_set_concentrations = args.input2
+    autofluorescence_set_concentrations = args.input3
+    # sample_volume = args.sample_volume
+    sample_volume = 10
 
-    input_df = input_importer(
-        input_file,
-        input_file_1,
-        input_file_2,
-        input_file_3)[0]
+    cfps_parameters_df = input_importer(
+        cfps_parameters,
+        initial_set_concentrations,
+        normalizer_set_concentrations,
+        autofluorescence_set_concentrations)[0]
 
-    input_df_1 = input_importer(
-        input_file,
-        input_file_1,
-        input_file_2,
-        input_file_3)[1]
+    initial_set_concentrations_df = input_importer(
+        cfps_parameters,
+        initial_set_concentrations,
+        normalizer_set_concentrations,
+        autofluorescence_set_concentrations)[1]
 
-    input_df_2 = input_importer(
-        input_file,
-        input_file_1,
-        input_file_2,
-        input_file_3)[2]
+    normalizer_set_concentrations_df = input_importer(
+        cfps_parameters,
+        initial_set_concentrations,
+        normalizer_set_concentrations,
+        autofluorescence_set_concentrations)[2]
 
-    input_df_3 = input_importer(
-        input_file,
-        input_file_1,
-        input_file_2,
-        input_file_3)[3]
+    autofluorescence_set_concentrations_df = input_importer(
+        cfps_parameters,
+        initial_set_concentrations,
+        normalizer_set_concentrations,
+        autofluorescence_set_concentrations)[3]
 
-    sample_volume = 10.5
-
-    intial_set_volumes = volumes_array_generator(
-        input_df,
-        input_df_1,
-        input_df_2,
-        input_df_3,
+    initial_set_volumes_df = volumes_array_generator(
+        cfps_parameters_df,
+        initial_set_concentrations_df,
+        normalizer_set_concentrations_df,
+        autofluorescence_set_concentrations_df,
         sample_volume)[0]
 
-    normalizer_set_volumes = volumes_array_generator(
-        input_df,
-        input_df_1,
-        input_df_2,
-        input_df_3,
+    normalizer_set_volumes_df = volumes_array_generator(
+        cfps_parameters_df,
+        initial_set_concentrations_df,
+        normalizer_set_concentrations_df,
+        autofluorescence_set_concentrations_df,
         sample_volume)[1]
 
-    autofluorescence_set_volumes = volumes_array_generator(
-        input_df,
-        input_df_1,
-        input_df_2,
-        input_df_3,
+    autofluorescence_set_volumes_df = volumes_array_generator(
+        cfps_parameters_df,
+        initial_set_concentrations_df,
+        normalizer_set_concentrations_df,
+        autofluorescence_set_concentrations_df,
         sample_volume)[2]
 
     save_volumes_array(
-        input_df,
-        intial_set_volumes,
-        normalizer_set_volumes,
-        autofluorescence_set_volumes)
+        cfps_parameters_df,
+        initial_set_volumes_df,
+        normalizer_set_volumes_df,
+        autofluorescence_set_volumes_df)
+
+    destination_plates_dict = destination_plate_generator(
+        initial_set_volumes_df,
+        normalizer_set_volumes_df,
+        autofluorescence_set_volumes_df,
+        starting_well='A1',
+        vertical=True)
+
+    echo_instructions_dict = echo_instructions_generator(
+        destination_plates_dict)
+
+    save_echo_instructions(echo_instructions_dict)
 
 
 if __name__ == "__main__":
