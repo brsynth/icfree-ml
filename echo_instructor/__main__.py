24402533a96d52c7b3b76a49267c5a2bc3574eb7
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2022-04-12 06:33:36 +0200

    Update __main__.py

--- echo_instructor/__main__.py
+++ echo_instructor/__main__.py
@@ -59,10 +59,29 @@ def main():
         starting_well='A1',
         vertical=True)
 
+<<<<<<< Updated upstream
     echo_instructions_dict = echo_instructions_generator(
         destination_plates_dict)
 
     save_echo_instructions(echo_instructions_dict)
+=======
+    multiple_echo_instructions_dict = \
+        multiple_echo_instructions_generator(multiple_destination_plates_dict)
+
+    single_destination_plates_dict = single_destination_plate_generator(
+        master_plate_1_final,
+        master_plate_2_final,
+        master_plate_3_final,
+        starting_well='A1',
+        vertical=True)
+
+    single_echo_instructions_dict = \
+        single_echo_instructions_generator(single_destination_plates_dict)
+
+    save_echo_instructions(
+        multiple_echo_instructions_dict,
+        single_echo_instructions_dict)
+>>>>>>> Stashed changes
 
 
 if __name__ == "__main__":
