37d86f309e4a60c0d6ad501cd61c128f62d969cb
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2022-04-15 12:08:55 +0200

    Update active_learning.py

--- active_learning/active_learning.py
+++ active_learning/active_learning.py
@@ -1,18 +1,21 @@
+import time
+import datetime
+import os
+import copy
+
 from sklearn import (
     linear_model,
     decomposition,
     neural_network,
     model_selection
-) 
+)
 
+from xgboost import (
+    XGBRegressor
+)
 from numpy import (
     concatenate,
     shape,
     reshape,
     amax
-
 )
-import time
-import datetime
-import os
-import copy
