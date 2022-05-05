33cc8206603a323fe27022b9881e1b018f702657
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2022-05-05 14:17:33 +0200

    Update packages.sh

--- packages.sh
+++ packages.sh
@@ -2,3 +2,6 @@ conda install -c conda-forge numpy
 conda install -c conda-forge pandas
 conda install -c conda-forge pydoe2
 conda install -c conda-forge brs_utils
+conda install -c anaconda scikit-learn
+conda install -c conda-forge xgboost
+
