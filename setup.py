4fcdbbf082a8e242acd713f3d62b3aea27b452e7
Joan Hérisson <joan.herisson@univ-evry.fr>
2022-05-24 17:48:52 +0200

    build(setup): fill

--- setup.py
+++ setup.py
@@ -1,21 +1,66 @@
+from os import path as os_path
+
 from setuptools import (
     setup,
     find_packages
     )
 
-# from os import path as os_path
+
+## INFOS ##
+package     = 'icfree-ml'
+descr       = 'Generate plates for cell-free buffer optimization'
+url         = 'https://github.com/brsynth/icfree-ml/'
+authors     = 'Yorgo El Moubayed, Joan Hérisson'
+corr_author = 'yorgo.el-moubayed@inrae.fr'
+
+
+## LONG DESCRIPTION
+with open(
+    os_path.join(
+        os_path.dirname(os_path.realpath(__file__)),
+        'README.md'
+    ),
+    'r',
+    encoding='utf-8'
+) as f:
+    long_description = f.read()
+
+def get_version():
+    with open(
+        os_path.join(
+            os_path.dirname(os_path.realpath(__file__)),
+            'CHANGELOG.md'
+        ),
+        'r'
+    ) as f:
+        lines = f.readlines()
+    for line in lines:
+        if line.startswith('##'):
+            from re import search
+            m = search("\[(.+)\]", line)
+            if m:
+                return m.group(1)
+
 
 setup(
-    name='plates_generator',
-    description='Generate plates for cell-free buffer optimization',
-    url='https://github.com/brsynth/icfree-ml/tree/main/plates_generator',
-    author='Yorgo El Moubayed',
-    install_requires=[
-        'os',
-        'numpy',
-        'pandas',
-        'pyDOE2'],
-    test_suite='pytest',
-    license='MIT',
-    packages=find_packages()
+    name                          = package,
+    version                       = get_version(),
+    author                        = authors,
+    author_email                  = corr_author,
+    description                   = descr,
+    long_description              = long_description,
+    long_description_content_type = 'text/markdown',
+    url                           = url,
+    packages                      = find_packages(),
+    package_dir                   = {package: package},
+    include_package_data          = True,
+    test_suite                    = 'pytest',
+    license                       = 'MIT',
+    classifiers                   = [
+        'Programming Language :: Python :: 3',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+    ],
+    python_requires               = '>=3.7',
 )
+
