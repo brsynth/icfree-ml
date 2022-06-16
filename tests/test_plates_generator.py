023abdde66000c11417dd44fe8299a5d258b75de
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2022-06-16 11:03:23 +0200

    chore: add test_plates_generator

--- tests/test_plates_generator.py
+++ tests/test_plates_generator.py
@@ -11,6 +11,7 @@ from pickle import (
 )
 from numpy.testing import (
     assert_array_equal
+    # assert_almost_equal
 )
 from pandas.testing import (
     assert_frame_equal
@@ -29,7 +30,9 @@ from json import (
 from icfree.plates_generator.plates_generator import (
     input_importer,
     input_processor,
-    doe_levels_generator
+    doe_levels_generator,
+    levels_to_concentrations,
+    plates_generator
 )
 
 
@@ -151,7 +154,133 @@ class Test(TestCase):
         pass
 
     def test_plates_generator(self):
-        pass
+        all_expected_columns = [
+            'Mg-glutamate',
+            'k-glutamate',
+            'CoA',
+            '3-PGA',
+            'NTP',
+            'NAD',
+            'Folinic acid',
+            'Spermidine',
+            'tRNA',
+            'Amino acids',
+            'CAMP',
+            'Extract',
+            'HEPES',
+            'PEG',
+            'Promoter',
+            'RBS',
+            'GFP-DNA',
+            'GOI-DNA'
+            ]
+
+        expected_columns_woGOI = [
+            'Mg-glutamate',
+            'k-glutamate',
+            'CoA',
+            '3-PGA',
+            'NTP',
+            'NAD',
+            'Folinic acid',
+            'Spermidine',
+            'tRNA',
+            'Amino acids',
+            'CAMP',
+            'Extract',
+            'HEPES',
+            'PEG',
+            'Promoter',
+            'RBS',
+            'GFP-DNA'
+            ]
+
+        # expected_doe_columns = [
+        #     'Mg-glutamate',
+        #     'k-glutamate',
+        #     'CoA',
+        #     '3-PGA',
+        #     'NTP',
+        #     'NAD',
+        #     'Folinic acid',
+        #     'Spermidine',
+        #     'tRNA',
+        #     'Amino acids',
+        #     'CAMP'
+        #     ]
+
+        # expected_const_columns = [
+        #     'HEPES',
+        #     'PEG',
+        #     'Promoter',
+        #     'RBS'
+        #     ]
+
+        # expected_dna_fluo_columns = 'GFP-DNA'
+        # expected_dna_goi_columns = 'GOI-DNA'
+
+        input_df = input_importer(os_path.join(
+                self.INPUT_FOLDER,
+                'proCFPS_parameters-tmp.tsv'
+                ))
+
+        parameters = input_processor(input_df)
+
+        n_variable_parameters = 11
+        doe_nb_concentrations = 5
+        doe_concentrations = np_append(
+            np_arange(0.0, 1.0, 1/(doe_nb_concentrations-1)),
+            1.0
+        )
+        doe_nb_samples = 10
+        seed = 123
+        doe_levels = doe_levels_generator(
+            n_variable_parameters=n_variable_parameters,
+            doe_nb_concentrations=doe_nb_concentrations,
+            doe_concentrations=doe_concentrations,
+            doe_nb_samples=doe_nb_samples,
+            seed=seed
+        )
+
+        max_conc = [
+            v['Maximum concentration']
+            for v in parameters['doe'].values()
+        ]
+        # Convert
+        doe_concentrations = levels_to_concentrations(
+            doe_levels,
+            max_conc,
+        )
+
+        dna_concentrations = {
+            v: dna_param[v]['Maximum concentration']
+            for status, dna_param in parameters.items()
+            for v in dna_param
+            if status.startswith('dna')
+        }
+
+        const_concentrations = {
+                k: v['Maximum concentration']
+                for k, v in parameters['const'].items()
+            }
+
+        plates = plates_generator(
+            doe_concentrations,
+            dna_concentrations,
+            const_concentrations,
+            parameters={k: list(v.keys()) for k, v in parameters.items()}
+        )
+
+        initial_set_df = plates['initial']
+        tested_columns = initial_set_df.columns.tolist()
+        # tested_doe_columns =
+        # tested_const_columns =
+        # tested_dna_fluo_columns =
+        # tested_dna_goi_columns =
+
+        self.assertListEqual(
+            all_expected_columns,
+            tested_columns)
 
     def test_save_plates(self):
         pass
