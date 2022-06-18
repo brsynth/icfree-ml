09fe275ab94a6b1529233b2cfe761dd9f4817440
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2022-06-19 00:08:37 +0200

    chore(test_echo_instructor): create _test_save_volumes
    create _test_save_volumes + test_save_volumes_wExistingOutFolder + test_save_volumes_woExistingOutFolder

--- tests/test_echo_instructor.py
+++ tests/test_echo_instructor.py
@@ -12,11 +12,15 @@ from pandas.testing import (
 from pandas import (
     read_json
 )
+from tempfile import (
+    TemporaryDirectory,
+    NamedTemporaryFile
+)
 
 from icfree.echo_instructor.echo_instructor import (
     input_importer,
-    volumes_array_generator
-    # save_volumes_array,
+    volumes_array_generator,
+    save_volumes
     # samples_merger,
     # multiple_destination_plate_generator,
     # multiple_echo_instructions_generator,
@@ -41,6 +45,11 @@ class Test(TestCase):
     REF_FOLDER = os_path.join(
         DATA_FOLDER,
         'output'
+    )
+
+    REF_FOLDER_VOLUMES = os_path.join(
+        DATA_FOLDER,
+        'output', 'volumes'
     )
 
     REF_FOLDER_INSTRUCTIONS = os_path.join(
@@ -241,8 +250,110 @@ class Test(TestCase):
             modulo_tested_normalizer_volumes_df
             )
 
-    def test_save_volumes_array(self):
-        pass
+    def test_save_volumes_wExistingOutFolder(self):
+        with TemporaryDirectory() as tmpFolder:
+            self._test_save_volumes(
+                output_folder=tmpFolder
+            )
+
+    def test_save_volumes_woExistingOutFolder(self):
+        self._test_save_volumes(
+            output_folder=NamedTemporaryFile().name
+        )
+
+    def _test_save_volumes(
+            self,
+            output_folder: str
+    ):
+        input_importer_dfs = input_importer(
+            self.tested_cfps_parameters,
+            self.tested_initial_concentrations,
+            self.tested_normalizer_concentrations,
+            self.tested_autofluorescence_concentrations)
+
+        tested_cfps_parameters_df = input_importer_dfs[0]
+        tested_initial_concentrations_df = input_importer_dfs[1]
+        tested_normalizer_concentrations_df = input_importer_dfs[2]
+        tested_autofluorescence_concentrations_df = input_importer_dfs[3]
+
+        volumes_array_generator_dfs = volumes_array_generator(
+            tested_cfps_parameters_df,
+            tested_initial_concentrations_df,
+            tested_normalizer_concentrations_df,
+            tested_autofluorescence_concentrations_df,
+            sample_volume=10000)
+
+        tested_initial_volumes_df = volumes_array_generator_dfs[0]
+        tested_normalizer_volumes_df = volumes_array_generator_dfs[1]
+        tested_autofluorescence_volumes_df = volumes_array_generator_dfs[2]
+
+        # LOAD REF FILES
+        ref_filename = 'expected_initial_volumes'
+        with open(
+            os_path.join(
+                    self.REF_FOLDER,
+                    f'{ref_filename}.tsv'
+            )
+        ) as fp1:
+            expected_initial_volumes = fp1.read()
+
+        ref_filename = 'expected_autofluorescence_volumes'
+        # if woGOI:
+        #     ref_filename += '_woGOI'
+        with open(
+            os_path.join(
+                    self.REF_FOLDER,
+                    f'{ref_filename}.tsv'
+            )
+        ) as fp2:
+            expected_autofluorescence_volumes = fp2.read()
+
+        ref_filename = 'expected_normalizer_volumes'
+        # if woGOI:
+        #     ref_filename += '_woGOI'
+        with open(
+            os_path.join(
+                    self.REF_FOLDER,
+                    f'{ref_filename}.tsv'
+            )
+        ) as fp3:
+            expected_normalizer_volumes = fp3.read()
+
+        save_volumes(
+            cfps_parameters_df=tested_cfps_parameters_df,
+            initial_volumes_df=tested_initial_volumes_df,
+            normalizer_volumes_df=tested_normalizer_volumes_df,
+            autofluorescence_volumes_df=tested_autofluorescence_volumes_df,
+            output_folder=output_folder)
+
+        with open(
+            os_path.join(
+                    self.REF_FOLDER,
+                    'tested_initial_volumes.tsv'
+            )
+        ) as fp4:
+            tested_initial_volumes = fp4.read()
+
+        with open(
+            os_path.join(
+                    self.REF_FOLDER,
+                    'tested_normalizer_volumes.tsv'
+            )
+        ) as fp5:
+            tested_normalizer_volumes = fp5.read()
+
+        with open(
+            os_path.join(
+                    self.REF_FOLDER,
+                    'tested_autofluorescence_volumes.tsv'
+            )
+        ) as fp6:
+            tested_autofluorescence_volumes = fp6.read()
+
+        assert expected_initial_volumes == tested_initial_volumes
+        assert expected_normalizer_volumes == tested_normalizer_volumes
+        assert expected_autofluorescence_volumes == \
+            tested_autofluorescence_volumes
 
     def test_samples_merger(self):
         pass
