647d354649d450753bfd33862fc6caf1fe656c2a
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2022-06-23 09:39:35 +0200

    Update test_echo_instructor.py

--- tests/test_echo_instructor.py
+++ tests/test_echo_instructor.py
@@ -29,8 +29,8 @@ from icfree.echo_instructor.echo_instructor import (
     distribute_destination_plate_generator,
     distribute_echo_instructions_generator,
     merge_destination_plate_generator,
-    merge_echo_instructions_generator
-    # save_echo_instructions
+    merge_echo_instructions_generator,
+    save_echo_instructions
 )
 
 
@@ -155,7 +155,7 @@ class Test(TestCase):
             check_dtype=False
         )
 
-    def test_volumes_array_generator(self):
+    def test_concentrations_to_volumes(self):
         input_importer_dfs = input_importer(
             self.tested_cfps_parameters,
             self.tested_initial_concentrations,
@@ -167,16 +167,16 @@ class Test(TestCase):
         tested_normalizer_concentrations_df = input_importer_dfs[2]
         tested_autofluorescence_concentrations_df = input_importer_dfs[3]
 
-        volumes_array_generator_dfs = concentrations_to_volumes(
+        concentrations_to_volumes_dfs = concentrations_to_volumes(
             tested_cfps_parameters_df,
             tested_initial_concentrations_df,
             tested_normalizer_concentrations_df,
             tested_autofluorescence_concentrations_df,
             sample_volume=10000)
 
-        tested_initial_volumes_df = volumes_array_generator_dfs[0]
-        tested_normalizer_volumes_df = volumes_array_generator_dfs[1]
-        tested_autofluorescence_volumes_df = volumes_array_generator_dfs[2]
+        tested_initial_volumes_df = concentrations_to_volumes_dfs[0]
+        tested_normalizer_volumes_df = concentrations_to_volumes_dfs[1]
+        tested_autofluorescence_volumes_df = concentrations_to_volumes_dfs[2]
 
         # Load references files
         with open(
@@ -186,7 +186,8 @@ class Test(TestCase):
             ), 'r'
         ) as fp1:
             expected_initial_volumes_df = read_json(
-                fp1, orient='split')
+                fp1,
+                orient='split')
 
         with open(
             os_path.join(
@@ -195,7 +196,8 @@ class Test(TestCase):
             ), 'r'
         ) as fp2:
             expected_normalizer_volumes_df = read_json(
-                fp2, orient='split')
+                fp2,
+                orient='split')
 
         with open(
             os_path.join(
@@ -204,7 +206,8 @@ class Test(TestCase):
             ), 'r'
         ) as fp3:
             expected_autofluorescence_volumes_df = read_json(
-                fp3, orient='split')
+                fp3,
+                orient='split')
 
         # Compare dataframes
         assert_frame_equal(
@@ -259,6 +262,53 @@ class Test(TestCase):
             modulo_expected_normalizer_volumes_df,
             modulo_tested_normalizer_volumes_df
             )
+
+    def test_concentrations_to_volumes_min_warning(
+        self,
+        output_folder
+    ):
+        input_importer_dfs = input_importer(
+            self.tested_cfps_parameters,
+            self.tested_initial_concentrations,
+            self.tested_normalizer_concentrations,
+            self.tested_autofluorescence_concentrations)
+
+        tested_cfps_parameters_df = input_importer_dfs[0]
+        tested_initial_concentrations_df = input_importer_dfs[1]
+        tested_normalizer_concentrations_df = input_importer_dfs[2]
+        tested_autofluorescence_concentrations_df = input_importer_dfs[3]
+
+        # Generate test log
+        concentrations_to_volumes(
+            tested_cfps_parameters_df,
+            tested_initial_concentrations_df,
+            tested_normalizer_concentrations_df,
+            tested_autofluorescence_concentrations_df,
+            sample_volume=10000)
+
+        # # Load test log
+        # with open(
+        #     os_path.join(
+        #             output_folder,
+        #             'volumes.log'
+        #     ), 'r'
+        # ) as fp:
+        #     expected_volumes_log = read_json(
+        #         fp,
+        #         orient='split')
+
+        # # Load reference log
+        # with open(
+        #     os_path.join(
+        #             self.REF_FOLDER,
+        #             'expected_volumes.log'
+        #     ), 'r'
+        # ) as fp:
+        #     expected_volumes_log = read_json(
+        #         fp,
+        #         orient='split')
+
+        # Compare Log files for minimum volumes warning
 
     def test_save_volumes_wExistingOutFolder(self):
         with TemporaryDirectory() as tmpFolder:
@@ -731,5 +781,208 @@ class Test(TestCase):
                 check_dtype=False
                 )
 
-    def test_save_echo_instructions(self):
-        pass
+    def test_save_echo_instructions_wExistingOutFolder(self):
+        with TemporaryDirectory() as tmpFolder:
+            self._test_save_echo_instructions(
+                output_folder=tmpFolder
+            )
+
+    def test_save_echo_instructions_woExistingOutFolder(self):
+        self._test_save_echo_instructions(
+            output_folder=NamedTemporaryFile().name
+        )
+
+    def _test_save_echo_instructions(
+            self,
+            output_folder: str
+            ):
+        input_importer_dfs = input_importer(
+            self.tested_cfps_parameters,
+            self.tested_initial_concentrations,
+            self.tested_normalizer_concentrations,
+            self.tested_autofluorescence_concentrations)
+
+        tested_cfps_parameters_df = input_importer_dfs[0]
+        tested_initial_concentrations_df = input_importer_dfs[1]
+        tested_normalizer_concentrations_df = input_importer_dfs[2]
+        tested_autofluorescence_concentrations_df = input_importer_dfs[3]
+
+        volumes_array_generator_dfs = concentrations_to_volumes(
+            tested_cfps_parameters_df,
+            tested_initial_concentrations_df,
+            tested_normalizer_concentrations_df,
+            tested_autofluorescence_concentrations_df,
+            sample_volume=10000)
+
+        tested_initial_volumes_df = volumes_array_generator_dfs[0]
+        tested_normalizer_volumes_df = volumes_array_generator_dfs[1]
+        tested_autofluorescence_volumes_df = volumes_array_generator_dfs[2]
+
+        samples_merger_dfs = samples_merger(
+            tested_initial_volumes_df,
+            tested_normalizer_volumes_df,
+            tested_autofluorescence_volumes_df)
+
+        tested_merged_plate_1_final = samples_merger_dfs[0]
+        tested_merged_plate_2_final = samples_merger_dfs[1]
+        tested_merged_plate_3_final = samples_merger_dfs[2]
+
+        tested_merge_destination_plates_dict =  \
+            merge_destination_plate_generator(
+                    tested_merged_plate_1_final,
+                    tested_merged_plate_2_final,
+                    tested_merged_plate_3_final,
+                    starting_well='A1',
+                    vertical=True)
+
+        tested_distribute_destination_plates_dict = \
+            distribute_destination_plate_generator(
+                    tested_initial_volumes_df,
+                    tested_normalizer_volumes_df,
+                    tested_autofluorescence_volumes_df,
+                    starting_well='A1',
+                    vertical=True)
+
+        tested_distribute_echo_instructions_dict = \
+            distribute_echo_instructions_generator(
+                tested_distribute_destination_plates_dict)
+
+        tested_merge_echo_instructions_dict = \
+            merge_echo_instructions_generator(
+                tested_merge_destination_plates_dict)
+
+        # Generate tested echo instructions files (distributed and merged)
+        save_echo_instructions(
+            tested_distribute_echo_instructions_dict,
+            tested_merge_echo_instructions_dict,
+            output_folder=output_folder)
+
+        # TEST MERGED ECHO INSTRUCTIONS FILES
+        # Load refrence files
+        ref_filename = 'expected_merged_initial_instructions.csv'
+        with open(
+            os_path.join(
+                    self.REF_FOLDER_INSTRUCTIONS,
+                    f'{ref_filename}.csv'
+            )
+        ) as fp7:
+            expected_merged_initial_instructions = fp7.read()
+
+        ref_filename = 'expected_merged_normalizer_instructions'
+        with open(
+            os_path.join(
+                    self.REF_FOLDER_INSTRUCTIONS,
+                    f'{ref_filename}.csv'
+            )
+        ) as fp8:
+            expected_merged_normalizer_instructions = fp8.read()
+
+        ref_filename = 'expected_merged_autofluorescence_instructions'
+        with open(
+            os_path.join(
+                    self.REF_FOLDER_INSTRUCTIONS,
+                    f'{ref_filename}.csv'
+            )
+        ) as fp9:
+            expected_merged_autofluorescence_instructions = fp9.read()
+
+        # Load tested merged echo instructions files
+        with open(
+            os_path.join(
+                    output_folder,
+                    'merged',
+                    'merged_initial_instructions.csv'
+            )
+        ) as fp10:
+            tested_merged_initial_instructions = fp10.read()
+
+        with open(
+            os_path.join(
+                    output_folder,
+                    'merged',
+                    'merged_normalizer_instructions.csv'
+            )
+        ) as fp11:
+            tested_merged_normalizer_instructions = fp11.read()
+
+        with open(
+            os_path.join(
+                    output_folder,
+                    'merged',
+                    'merged_autofluorescence_instructions.csv'
+            )
+        ) as fp12:
+            tested_merged_autofluorescence_instructions = fp12.read()
+
+        # Compare merged echo instructions files
+        assert expected_merged_initial_instructions == \
+            tested_merged_initial_instructions
+        assert expected_merged_normalizer_instructions == \
+            tested_merged_normalizer_instructions
+        assert expected_merged_autofluorescence_instructions == \
+            tested_merged_autofluorescence_instructions
+
+        # TEST DISTRBUTED ECHO INSTRUCTIONS FILES
+        # Load refrence files
+        ref_filename = 'expected_distributed_initial_instructions'
+        with open(
+            os_path.join(
+                    self.REF_FOLDER_INSTRUCTIONS,
+                    'distributed',
+                    f'{ref_filename}.csv'
+            )
+        ) as fp1:
+            expected_distributed_initial_instructions = fp1.read()
+
+        ref_filename = 'expected_distributed_normalizer_instructions'
+        with open(
+            os_path.join(
+                    self.REF_FOLDER_INSTRUCTIONS,
+                    'distributed',
+                    f'{ref_filename}.csv'
+            )
+        ) as fp2:
+            expected_distributed_normalizer_instructions = fp2.read()
+
+        ref_filename = 'expected_distributed_autofluorescence_instructions'
+        with open(
+            os_path.join(
+                    self.REF_FOLDER_INSTRUCTIONS,
+                    'distributed',
+                    f'{ref_filename}.csv'
+            )
+        ) as fp3:
+            expected_distributed_autofluorescence_instructions = fp3.read()
+
+        # Load tested distributed echo instructions files
+        with open(
+            os_path.join(
+                    output_folder,
+                    'distributed_initial_instructions.csv'
+            )
+        ) as fp4:
+            tested_distributed_initial_instructions = fp4.read()
+
+        with open(
+            os_path.join(
+                    output_folder,
+                    'distributed_normalizer_instructions.csv'
+            )
+        ) as fp5:
+            tested_distributed_normalizer_instructions = fp5.read()
+
+        with open(
+            os_path.join(
+                    output_folder,
+                    'distributed_autofluorescence_instructions.csv'
+            )
+        ) as fp6:
+            tested_distributed_autofluorescence_instructions = fp6.read()
+
+        # Compare distributed echo instructions files
+        assert expected_distributed_initial_instructions == \
+            tested_distributed_initial_instructions
+        assert expected_distributed_normalizer_instructions == \
+            tested_distributed_normalizer_instructions
+        assert expected_distributed_autofluorescence_instructions == \
+            tested_distributed_autofluorescence_instructions
