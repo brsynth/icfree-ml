86eb6a60008936f5c6da95a85e5f3c944fedf754
yorgomoubayed <41142299+yorgomoubayed@users.noreply.github.com>
2022-06-29 12:14:30 +0200

    tests: update concentrations_to_volumes tests

--- tests/test_echo_instructor.py
+++ tests/test_echo_instructor.py
@@ -21,6 +21,7 @@ from tempfile import (
     NamedTemporaryFile
 )
 
+
 from icfree.echo_instructor.echo_instructor import (
     input_importer,
     concentrations_to_volumes,
@@ -90,7 +91,8 @@ class Test(TestCase):
             ), 'r'
         ) as fp:
             expected_cfps_parameters_df = read_json(
-                fp, orient='split')
+                fp,
+                orient='split')
 
         with open(
             os_path.join(
@@ -99,7 +101,8 @@ class Test(TestCase):
             ), 'r'
         ) as fp:
             expected_initial_concentrations_df = read_json(
-                fp, orient='split')
+                fp,
+                orient='split')
 
         with open(
             os_path.join(
@@ -108,7 +111,8 @@ class Test(TestCase):
             ), 'r'
         ) as fp:
             expected_normalizer_concentrations_df = read_json(
-                fp, orient='split')
+                fp,
+                orient='split')
 
         with open(
             os_path.join(
@@ -117,7 +121,8 @@ class Test(TestCase):
             ), 'r'
         ) as fp:
             expected_autofluorescence_concentrations_df = read_json(
-                fp, orient='split')
+                fp,
+                orient='split')
 
             input_importer_dfs = input_importer(
                 self.tested_cfps_parameters,
@@ -130,7 +135,7 @@ class Test(TestCase):
             tested_normalizer_concentrations_df = input_importer_dfs[2]
             tested_autofluorescence_concentrations_df = input_importer_dfs[3]
 
-        # Compare dataframes
+        # Compare dataframes while ignoring data types
         assert_frame_equal(
             expected_cfps_parameters_df,
             tested_cfps_parameters_df,
@@ -167,16 +172,28 @@ class Test(TestCase):
         tested_normalizer_concentrations_df = input_importer_dfs[2]
         tested_autofluorescence_concentrations_df = input_importer_dfs[3]
 
-        concentrations_to_volumes_dfs = concentrations_to_volumes(
+        concentrations_to_volumes_variables = concentrations_to_volumes(
             tested_cfps_parameters_df,
             tested_initial_concentrations_df,
             tested_normalizer_concentrations_df,
             tested_autofluorescence_concentrations_df,
-            sample_volume=10000)
+            sample_volume=10000,
+            source_plate_dead_volume=15000)
 
-        tested_initial_volumes_df = concentrations_to_volumes_dfs[0]
-        tested_normalizer_volumes_df = concentrations_to_volumes_dfs[1]
-        tested_autofluorescence_volumes_df = concentrations_to_volumes_dfs[2]
+        tested_initial_volumes_df = \
+            concentrations_to_volumes_variables[0]
+        tested_normalizer_volumes_df = \
+            concentrations_to_volumes_variables[1]
+        tested_autofluorescence_volumes_df = \
+            concentrations_to_volumes_variables[2]
+        tested_initial_volumes_summary_df = \
+            concentrations_to_volumes_variables[3]
+        tested_normalizer_volumes_summary_df = \
+            concentrations_to_volumes_variables[4]
+        tested_autofluorescence_volumes_summary_df = \
+            concentrations_to_volumes_variables[5]
+        tested_warning_volumes_report_df = \
+            concentrations_to_volumes_variables[6]
 
         # Load references files
         with open(
@@ -209,7 +226,47 @@ class Test(TestCase):
                 fp,
                 orient='split')
 
-        # Compare dataframes
+        with open(
+            os_path.join(
+                    self.REF_FOLDER,
+                    'expected_initial_volumes_summary_df.json'
+            ), 'r'
+        ) as fp:
+            expected_initial_volumes_summary_df = read_json(
+                fp,
+                orient='split')
+
+        with open(
+            os_path.join(
+                    self.REF_FOLDER,
+                    'expected_normalizer_volumes_summary_df.json'
+            ), 'r'
+        ) as fp:
+            expected_normalizer_volumes_summary_df = read_json(
+                fp,
+                orient='split')
+
+        with open(
+            os_path.join(
+                    self.REF_FOLDER,
+                    'expected_autofluorescence_volumes_summary_df.json'
+            ), 'r'
+        ) as fp:
+            expected_autofluorescence_volumes_summary_df = read_json(
+                fp,
+                orient='split')
+
+        with open(
+            os_path.join(
+                    self.REF_FOLDER,
+                    'expected_warning_volumes_report_df.json'
+            ), 'r'
+        ) as fp:
+            expected_warning_volumes_report_df = read_json(
+                fp,
+                orient='split')
+
+        # Compare dataframes while ignoring data types
         assert_frame_equal(
             expected_initial_volumes_df,
             tested_initial_volumes_df,
@@ -228,7 +285,31 @@ class Test(TestCase):
             check_dtype=False
             )
 
-        # Compare dataframes modulo
+        assert_frame_equal(
+            expected_initial_volumes_summary_df,
+            tested_initial_volumes_summary_df,
+            check_dtype=False
+            )
+
+        assert_frame_equal(
+            expected_normalizer_volumes_summary_df,
+            tested_normalizer_volumes_summary_df,
+            check_dtype=False
+            )
+
+        assert_frame_equal(
+            expected_autofluorescence_volumes_summary_df,
+            tested_autofluorescence_volumes_summary_df,
+            check_dtype=False
+            )
+
+        assert_frame_equal(
+            expected_warning_volumes_report_df,
+            tested_warning_volumes_report_df,
+            check_dtype=False
+            )
+
+        # Generate volumes dataframes modulo
         modulo_expected_initial_volumes_df = \
             expected_initial_volumes_df % 2.5
 
@@ -247,7 +328,7 @@ class Test(TestCase):
         modulo_tested_autofluorescence_volumes_df = \
             tested_autofluorescence_volumes_df % 2.5
 
-        # Compare dataframes
+        # Compare volumes dataframes modulo
         assert_frame_equal(
             modulo_expected_initial_volumes_df,
             modulo_tested_initial_volumes_df
@@ -263,16 +344,7 @@ class Test(TestCase):
             modulo_tested_normalizer_volumes_df
             )
 
-    def test_concentrations_to_volumes_min_warning_factor(self):
-        pass
-
-    def test_concentrations_to_volumes_min_warning_water(self):
-        pass
-
-    def test_concentrations_to_volumes_max_warning_factor(self):
-        pass
-
-    def test_concentrations_to_volumes_max_warning_water(self):
+    def test_concentrations_to_volumes_warnings(self):
         pass
 
     def test_save_volumes_wExistingOutFolder(self):
@@ -301,20 +373,28 @@ class Test(TestCase):
         tested_normalizer_concentrations_df = input_importer_dfs[2]
         tested_autofluorescence_concentrations_df = input_importer_dfs[3]
 
-        concentrations_to_volumes_dfs = concentrations_to_volumes(
+        concentrations_to_volumes_variables = concentrations_to_volumes(
             tested_cfps_parameters_df,
             tested_initial_concentrations_df,
             tested_normalizer_concentrations_df,
             tested_autofluorescence_concentrations_df,
-            sample_volume=10000)
+            sample_volume=10000,
+            source_plate_dead_volume=15000)
 
-        tested_initial_volumes_df = concentrations_to_volumes_dfs[0]
-        tested_normalizer_volumes_df = concentrations_to_volumes_dfs[1]
-        tested_autofluorescence_volumes_df = concentrations_to_volumes_dfs[2]
-        tested_initial_volumes_summary = concentrations_to_volumes_dfs[3]
-        tested_normalizer_volumes_summary = concentrations_to_volumes_dfs[4]
-        tested_autofluorescence_volumes_summary = \
-            concentrations_to_volumes_dfs[5]
+        tested_initial_volumes_df = \
+            concentrations_to_volumes_variables[0]
+        tested_normalizer_volumes_df = \
+            concentrations_to_volumes_variables[1]
+        tested_autofluorescence_volumes_df = \
+            concentrations_to_volumes_variables[2]
+        tested_initial_volumes_summary_df = \
+            concentrations_to_volumes_variables[3]
+        tested_normalizer_volumes_summary_df = \
+            concentrations_to_volumes_variables[4]
+        tested_autofluorescence_volumes_summary_df = \
+            concentrations_to_volumes_variables[5]
+        tested_warning_volumes_report_df = \
+            concentrations_to_volumes_variables[6]
 
         # Load volumes refrence files
         ref_filename = 'expected_initial_volumes'
@@ -372,15 +452,25 @@ class Test(TestCase):
         ) as fp:
             expected_normalizer_volumes_summary = fp.read()
 
+        ref_filename = 'expected_warning_volumes_report'
+        with open(
+            os_path.join(
+                    self.REF_FOLDER_VOLUMES,
+                    f'{ref_filename}.tsv'
+            )
+        ) as fp:
+            expected_warning_volumes_report = fp.read()
+
         # Generate test volume files
         save_volumes(
             tested_cfps_parameters_df,
             tested_initial_volumes_df,
             tested_normalizer_volumes_df,
             tested_autofluorescence_volumes_df,
-            tested_initial_volumes_summary,
-            tested_normalizer_volumes_summary,
-            tested_autofluorescence_volumes_summary,
+            tested_initial_volumes_summary_df,
+            tested_normalizer_volumes_summary_df,
+            tested_autofluorescence_volumes_summary_df,
+            tested_warning_volumes_report_df,
             output_folder=output_folder)
 
         # Load test volume files
@@ -438,6 +528,15 @@ class Test(TestCase):
         ) as fp:
             tested_autofluorescence_volumes_summary = fp.read()
 
+        with open(
+            os_path.join(
+                    output_folder,
+                    'volumes_output',
+                    'warning_volumes_report.tsv'
+            )
+        ) as fp:
+            tested_warning_volumes_report = fp.read()
+
         # Compare volumes files
         assert expected_initial_volumes == tested_initial_volumes
         assert expected_normalizer_volumes == tested_normalizer_volumes
@@ -452,6 +551,10 @@ class Test(TestCase):
         assert expected_autofluorescence_volumes_summary == \
             tested_autofluorescence_volumes_summary
 
+        # Compare warning volumes reports
+        assert expected_warning_volumes_report == \
+            tested_warning_volumes_report
+
     def test_samples_merger(self):
         input_importer_dfs = input_importer(
             self.tested_cfps_parameters,
@@ -464,16 +567,18 @@ class Test(TestCase):
         tested_normalizer_concentrations_df = input_importer_dfs[2]
         tested_autofluorescence_concentrations_df = input_importer_dfs[3]
 
-        volumes_array_generator_dfs = concentrations_to_volumes(
+        concentrations_to_volumes_variables = concentrations_to_volumes(
             tested_cfps_parameters_df,
             tested_initial_concentrations_df,
             tested_normalizer_concentrations_df,
             tested_autofluorescence_concentrations_df,
-            sample_volume=10000)
+            sample_volume=10000,
+            source_plate_dead_volume=15000)
 
-        tested_initial_volumes_df = volumes_array_generator_dfs[0]
-        tested_normalizer_volumes_df = volumes_array_generator_dfs[1]
-        tested_autofluorescence_volumes_df = volumes_array_generator_dfs[2]
+        tested_initial_volumes_df = concentrations_to_volumes_variables[0]
+        tested_normalizer_volumes_df = concentrations_to_volumes_variables[1]
+        tested_autofluorescence_volumes_df = \
+            concentrations_to_volumes_variables[2]
 
         samples_merger_dfs = samples_merger(
             tested_initial_volumes_df,
@@ -546,16 +651,18 @@ class Test(TestCase):
         tested_normalizer_concentrations_df = input_importer_dfs[2]
         tested_autofluorescence_concentrations_df = input_importer_dfs[3]
 
-        volumes_array_generator_dfs = concentrations_to_volumes(
+        concentrations_to_volumes_variables = concentrations_to_volumes(
             tested_cfps_parameters_df,
             tested_initial_concentrations_df,
             tested_normalizer_concentrations_df,
             tested_autofluorescence_concentrations_df,
-            sample_volume=10000)
+            sample_volume=10000,
+            source_plate_dead_volume=15000)
 
-        tested_initial_volumes_df = volumes_array_generator_dfs[0]
-        tested_normalizer_volumes_df = volumes_array_generator_dfs[1]
-        tested_autofluorescence_volumes_df = volumes_array_generator_dfs[2]
+        tested_initial_volumes_df = concentrations_to_volumes_variables[0]
+        tested_normalizer_volumes_df = concentrations_to_volumes_variables[1]
+        tested_autofluorescence_volumes_df = \
+            concentrations_to_volumes_variables[2]
 
         tested_distribute_destination_plates_dict = \
             distribute_destination_plate_generator(
@@ -611,16 +718,18 @@ class Test(TestCase):
         tested_normalizer_concentrations_df = input_importer_dfs[2]
         tested_autofluorescence_concentrations_df = input_importer_dfs[3]
 
-        volumes_array_generator_dfs = concentrations_to_volumes(
+        concentrations_to_volumes_variables = concentrations_to_volumes(
             tested_cfps_parameters_df,
             tested_initial_concentrations_df,
             tested_normalizer_concentrations_df,
             tested_autofluorescence_concentrations_df,
-            sample_volume=10000)
+            sample_volume=10000,
+            source_plate_dead_volume=15000)
 
-        tested_initial_volumes_df = volumes_array_generator_dfs[0]
-        tested_normalizer_volumes_df = volumes_array_generator_dfs[1]
-        tested_autofluorescence_volumes_df = volumes_array_generator_dfs[2]
+        tested_initial_volumes_df = concentrations_to_volumes_variables[0]
+        tested_normalizer_volumes_df = concentrations_to_volumes_variables[1]
+        tested_autofluorescence_volumes_df = \
+            concentrations_to_volumes_variables[2]
 
         tested_distribute_destination_plates_dict = \
             distribute_destination_plate_generator(
@@ -680,16 +789,18 @@ class Test(TestCase):
         tested_normalizer_concentrations_df = input_importer_dfs[2]
         tested_autofluorescence_concentrations_df = input_importer_dfs[3]
 
-        volumes_array_generator_dfs = concentrations_to_volumes(
+        concentrations_to_volumes_variables = concentrations_to_volumes(
             tested_cfps_parameters_df,
             tested_initial_concentrations_df,
             tested_normalizer_concentrations_df,
             tested_autofluorescence_concentrations_df,
-            sample_volume=10000)
+            sample_volume=10000,
+            source_plate_dead_volume=15000)
 
-        tested_initial_volumes_df = volumes_array_generator_dfs[0]
-        tested_normalizer_volumes_df = volumes_array_generator_dfs[1]
-        tested_autofluorescence_volumes_df = volumes_array_generator_dfs[2]
+        tested_initial_volumes_df = concentrations_to_volumes_variables[0]
+        tested_normalizer_volumes_df = concentrations_to_volumes_variables[1]
+        tested_autofluorescence_volumes_df = \
+            concentrations_to_volumes_variables[2]
 
         samples_merger_dfs = samples_merger(
             tested_initial_volumes_df,
@@ -752,16 +863,18 @@ class Test(TestCase):
         tested_normalizer_concentrations_df = input_importer_dfs[2]
         tested_autofluorescence_concentrations_df = input_importer_dfs[3]
 
-        volumes_array_generator_dfs = concentrations_to_volumes(
+        concentrations_to_volumes_variables = concentrations_to_volumes(
             tested_cfps_parameters_df,
             tested_initial_concentrations_df,
             tested_normalizer_concentrations_df,
             tested_autofluorescence_concentrations_df,
-            sample_volume=10000)
+            sample_volume=10000,
+            source_plate_dead_volume=15000)
 
-        tested_initial_volumes_df = volumes_array_generator_dfs[0]
-        tested_normalizer_volumes_df = volumes_array_generator_dfs[1]
-        tested_autofluorescence_volumes_df = volumes_array_generator_dfs[2]
+        tested_initial_volumes_df = concentrations_to_volumes_variables[0]
+        tested_normalizer_volumes_df = concentrations_to_volumes_variables[1]
+        tested_autofluorescence_volumes_df = \
+            concentrations_to_volumes_variables[2]
 
         samples_merger_dfs = samples_merger(
             tested_initial_volumes_df,
@@ -842,16 +955,18 @@ class Test(TestCase):
         tested_normalizer_concentrations_df = input_importer_dfs[2]
         tested_autofluorescence_concentrations_df = input_importer_dfs[3]
 
-        volumes_array_generator_dfs = concentrations_to_volumes(
+        concentrations_to_volumes_variables = concentrations_to_volumes(
             tested_cfps_parameters_df,
             tested_initial_concentrations_df,
             tested_normalizer_concentrations_df,
             tested_autofluorescence_concentrations_df,
-            sample_volume=10000)
+            sample_volume=10000,
+            source_plate_dead_volume=15000)
 
-        tested_initial_volumes_df = volumes_array_generator_dfs[0]
-        tested_normalizer_volumes_df = volumes_array_generator_dfs[1]
-        tested_autofluorescence_volumes_df = volumes_array_generator_dfs[2]
+        tested_initial_volumes_df = concentrations_to_volumes_variables[0]
+        tested_normalizer_volumes_df = concentrations_to_volumes_variables[1]
+        tested_autofluorescence_volumes_df = \
+            concentrations_to_volumes_variables[2]
 
         samples_merger_dfs = samples_merger(
             tested_initial_volumes_df,
